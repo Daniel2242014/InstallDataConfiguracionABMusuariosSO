afa76f2a90315a0186487704bfc2b8a0b0ba78d0
Daniel2242014 <40508304+Daniel2242014@users.noreply.github.com>
2019-09-03 22:30:21 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -81,6 +81,7 @@ EOF
 		echo "Bienvenido al servidor del sistema SLTA" > /etc/issue #Cargamos issue para el aviso previo al logeo 
 		echo "Ingrese su usuario y contraseña" >> /etc/issue
 
+
 		verifMenu=-1
 
 		source /var/DataConfiguracionABMusuariosSO/sub_shell/configurarRed.sh
@@ -92,7 +93,6 @@ EOF
 		systemctl disable NetworkManager
 		yum remove NetworkManager firewalld
 		yum install policycoreutils-python git
-
 		if ! test -d /opt/IBM
 		then
 			echo "¿Desea ademas instalar el gestor de base de datos Informix? [1=si, 0=no]"
@@ -132,7 +132,7 @@ then
 		then
 			if test -f /var/DataConfiguracionABMusuariosSO/I_Inxo
 			then
-               source /var/DataConfiguracionABMusuariosSO/Informix_install2.sh 
+              source informix_install2.sh 
 			fi 
 		    source /var/DataConfiguracionABMusuariosSO/adm_tool.sh
 		else
